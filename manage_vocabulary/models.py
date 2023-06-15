4eec33ed2c936d06da7b942ae35570a85eeaf58c
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-06-14 20:10:11 -0700

    add new databse tables

--- manage_vocabulary/models.py
+++ manage_vocabulary/models.py
@@ -5,3 +5,17 @@ from django.utils import timezone
 
 class User(AbstractUser):
     pass
+
+class PartOfSpeech(models.Model):
+    WORD_TYPES = [
+        ('verb', 'Verb'),
+        ('noun', 'Noun'),
+        ('adjective', 'Adjective'),
+        ('phrasal_verb', 'Phrasal Verb'),
+    ]
+    word_type = models.CharField(max_length=20, choices=WORD_TYPES)
+    definition = models.CharField(max_length=300)
+    example = models.TextField()
+class Word(models.Model):
+    name = models.CharField(max_length=100)
+    word_types = models.ManyToManyField(PartOfSpeech, blank=True, related_name="word_types")
