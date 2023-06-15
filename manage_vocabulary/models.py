fb9cc22ca8cf84a63eb65f45e2d08c2d390d5ec7
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-06-14 21:09:17 -0700

    edit tables relationship

--- manage_vocabulary/models.py
+++ manage_vocabulary/models.py
@@ -6,6 +6,10 @@ from django.utils import timezone
 class User(AbstractUser):
     pass
 
+class Group(models.Model):
+    definition = models.CharField(max_length=300)
+    example = models.TextField()
+
 class PartOfSpeech(models.Model):
     WORD_TYPES = [
         ('verb', 'Verb'),
@@ -14,8 +18,8 @@ class PartOfSpeech(models.Model):
         ('phrasal_verb', 'Phrasal Verb'),
     ]
     word_type = models.CharField(max_length=20, choices=WORD_TYPES)
-    definition = models.CharField(max_length=300)
-    example = models.TextField()
+    groups = models.ManyToManyField(Group, blank=True, related_name="groups")
+    
 class Word(models.Model):
     name = models.CharField(max_length=100)
     word_types = models.ManyToManyField(PartOfSpeech, blank=True, related_name="word_types")
