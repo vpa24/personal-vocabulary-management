597da1ea9cf439dfa7ccdc698857ec8d858ec9b9
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-06-18 14:30:04 -0700

    fixed bug for add vocabulary function

--- manage_vocabulary/views.py
+++ manage_vocabulary/views.py
@@ -3,19 +3,21 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.shortcuts import get_object_or_404
 from collections import defaultdict
+from django.shortcuts import get_object_or_404
 
 from .models import User, Word, WordEntry
 from django.contrib.auth.decorators import login_required
 from .addVocabularyForm import addVocabularyForm
 
+
 def index(request):
     if request.user.is_authenticated:
         context = vocabulary_list(request)
-        return render(request,'manage_vocabulary/index_user_is_authenticated.html', context)
+        return render(request, 'manage_vocabulary/index_user_is_authenticated.html', context)
     else:
         return render(request, 'manage_vocabulary/index.html')
+
 
 def login_view(request):
     if request.method == "POST":
@@ -68,7 +70,8 @@ def register(request):
     else:
         return render(request, "manage_vocabulary/register.html")
 
-@login_required()    
+
+@login_required()
 def add_vocabulary(request):
     if request.method == 'POST':
         form = addVocabularyForm(request.POST)
@@ -76,37 +79,22 @@ def add_vocabulary(request):
             name = request.POST['name'].lower().strip()
             user_id = request.user.id
             user = User.objects.get(pk=user_id)
-             # Check if the vocabulary exists for the current user
-            existing_vocabulary = user.words.filter(name=name)
-            if existing_vocabulary.exists():
-                vid = existing_vocabulary.first().id
+            vid = existing_vocabulary(user, name)
+            if vid != None:
                 return render(request, 'manage_vocabulary/add_vocabulary.html', {'form': form, 'message': 'This vocabulary already exists. If you want to view or edit it', 'id': vid})
-            
-             # Check if the vocabulary exists with different owners
-            shared_vocabulary = Word.objects.filter(name=name).exclude(owners=user)
-            if shared_vocabulary.exists():
-                shared_word = shared_vocabulary.first()
-                shared_word.owners.add(user)
-                vid = shared_word.id
-                if vid is not None:
-                    word = Word.objects.get(pk=vid)
-                    add_to_word_entry(request, word)
-                    return HttpResponseRedirect(reverse("index"))
-            
-            user = User.objects.get(pk=user_id)
-            new_word = Word(name=name)
-            new_word.save()
-            new_word.owners.set([user])
-            add_to_word_entry(request, new_word)
-            return HttpResponseRedirect(reverse("index"))
+            elif shared_vocabulary(user, name, request):
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                add_new_vocabulary(user_id, name, request)
     else:
         form = addVocabularyForm()
         return render(request, 'manage_vocabulary/add_vocabulary.html', {'form': form})
-    
+
+
 def vocabulary_list(request):
     user_id = request.user.id
     user = User.objects.get(pk=user_id)
-    vocabulary_words = Word.objects.filter(owners = user)
+    vocabulary_words = Word.objects.filter(owners=user)
     word_dict = defaultdict(list)
     for word in vocabulary_words:
         first_letter = word.name[0].upper()
@@ -123,22 +111,27 @@ def vocabulary_list(request):
     }
     return context
 
+
 @login_required()
 def vocabulary_detail(request, vid):
-    word = Word.objects.get(pk=vid)
-    word_entries = WordEntry.objects.filter(word=word)
-    return render(request, 'manage_vocabulary/vocabulary_detail.html' ,{
+    user = request.user
+    word = get_object_or_404(Word, id=vid)
+
+    word_entries = word.word_entries.filter(word__owners=user)
+    return render(request, 'manage_vocabulary/vocabulary_detail.html', {
         'word': word,
         'word_entries': word_entries
     })
 
+
 def vocabulary_id_by_user(name, request):
     user_id = request.user.id
-    user =  User.objects.get(pk=user_id)
+    user = User.objects.get(pk=user_id)
     word = Word.objects.get(name=name, owner=user)
     if word:
         return word.id
     return 0
+
 
 def get_owners_by_vocabulary(name):
     words = Word.objects.filter(name=name)
@@ -146,12 +139,13 @@ def get_owners_by_vocabulary(name):
         word = words.first()
         return word.owners.all()
     return None
-    
+
 
 def add_new_owner(user_id, name):
     new_owner = User.objects.get(pk=user_id)
-    word =Word.objects.get(name=name)
+    word = Word.objects.get(name=name)
     word.owners.add(new_owner)
+
 
 def add_to_word_entry(request, word):
     definitions = request.POST.getlist('definition')
@@ -169,3 +163,37 @@ def add_to_word_entry(request, word):
             word=word
         )
         word_entry.save()
+
+
+def existing_vocabulary(user, name):
+    # Check if the vocabulary exists for the current user
+    existing_vocabulary = user.words.filter(name=name)
+    if existing_vocabulary.exists():
+        vid = existing_vocabulary.first().id
+        return vid
+    else:
+        return None
+
+
+def shared_vocabulary(user, name, request):
+    # Check if the vocabulary exists with different owners
+    shared_vocabulary = Word.objects.filter(name=name).exclude(owners=user)
+    if shared_vocabulary.exists():
+        shared_word = shared_vocabulary.first()
+        shared_word.owners.add(user)
+        vid = shared_word.id
+        if vid is not None:
+            word = Word.objects.get(pk=vid)
+            add_to_word_entry(request, word)
+            return True
+    else:
+        return False
+
+
+def add_new_vocabulary(user_id, name, request):
+    user = User.objects.get(pk=user_id)
+    new_word = Word(name=name)
+    new_word.save()
+    new_word.owners.set([user])
+    add_to_word_entry(request, new_word)
+    return HttpResponseRedirect(reverse("index"))
