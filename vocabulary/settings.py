60be5999afcfb9ddb1b79446c55d7ece8e0339ab
Phuong Anh Vu <vuphuonganh020497@gmail.com>
2023-07-01 22:04:40 -0700

    Update settings.py

--- vocabulary/settings.py
+++ vocabulary/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'vocabulary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vocabulary',
+        'USER': 'admin',
+        'PASSWORD': 'aP4H7zvVwMmuXWElIg5frQII3iIbPxXN',
+        'HOST': 'mz62am.stackhero-network.com',
+        'PORT': '5432',
     }
 }
 
