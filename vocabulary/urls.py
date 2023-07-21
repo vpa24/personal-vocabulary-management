27f75a5476efc3e4a2046da16085053fc8d7a4c9
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-07-20 21:42:24 -0700

    oauth/ for social login

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("manage_vocabulary.urls"))
+    path("", include("manage_vocabulary.urls")),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
