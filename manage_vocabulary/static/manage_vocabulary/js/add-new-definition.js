365853bd23ea3807cdd03ea5c66e720c693424a4
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-07-26 20:09:45 -0700

    add missing js files

---
+++ manage_vocabulary/static/manage_vocabulary/js/add-new-definition.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var addButton = document.getElementById("add_other_definition");
+  addButton.addEventListener("click", function () {
+    // Select the target div to clone
+    var targetDiv = document.querySelector(
+      ".row.mt-3.p-4.bg-secondary.rounded"
+    );
+
+    if (targetDiv) {
+      // Clone the target div with its content
+      var clonedDiv = targetDiv.cloneNode(true);
+
+      // Clear any user input values in the cloned div
+      var clonedInputs = clonedDiv.querySelectorAll("input, textarea");
+      clonedInputs.forEach(function (input) {
+        input.value = "";
+      });
+
+      // Append the cloned div after the last occurrence of the target div
+      targetDiv.parentNode.appendChild(clonedDiv);
+    }
+  });
+});
