9fc9d84ae68183d7574f23a760e1ff63192f2e1c
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-08-27 22:08:58 -0700

    temporary move template to another directory

--- vocabulary/settings.py
+++ vocabulary/settings.py
@@ -172,6 +172,8 @@ SECURE_SSL_REDIRECT = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'vocabulary/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
