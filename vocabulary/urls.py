c9f4282e8e554f46cedf1bf7ce92851c2ab45ae0
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-08-27 21:42:55 -0700

    create new authentication function

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("manage_vocabulary.urls")),
+    path('authentication/', include('authentication.urls')),
     path('accounts/', include('allauth.urls')),
 ]
