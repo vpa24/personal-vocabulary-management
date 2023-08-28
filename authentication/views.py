ac05c24ae9fd598e7ce526115edafc8f48853004
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-08-27 21:51:20 -0700

    add validate email lib

--- authentication/views.py
+++ authentication/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
-# from validate_email import validate_email
+from validate_email import validate_email
 from django.contrib import messages
 from django.core.mail import EmailMessage
 from django.contrib.sites.shortcuts import get_current_site
@@ -26,8 +26,8 @@ class EmailValidationView(View):
     def post(self, request):
         data = json.loads(request.body)
         email = data['email']
-        # if not validate_email(email):
-        #     return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
         if User.objects.filter(email=email).exists():
             return JsonResponse({'email_error': 'sorry email in use,choose another one '}, status=409)
         return JsonResponse({'email_valid': True})
