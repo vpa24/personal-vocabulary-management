f5dda6fadf8d35b5351fbe14d056cd5595e5cd59
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-09-26 17:22:40 -0700

    complete send daily mail function

--- manage_vocabulary/management/commands/send-mail.py
+++ manage_vocabulary/management/commands/send-mail.py
@@ -1,16 +1,59 @@
 from typing import Any
 from django.core.management.base import BaseCommand
-from manage_vocabulary.models import User, Word, WordEntry
-from datetime import timedelta, date
+from manage_vocabulary.models import User, Word, WordEntry, WordOwnership
+from django.core.mail import send_mail
+from django.conf import settings
+import random
+from datetime import timedelta, date, datetime
+from django.utils.html import format_html
+from django.core.mail import EmailMessage
+
 
 class Command(BaseCommand):
-	def handle(self, *arg, **kwargs):
-		today = date.today()
-		users = User.objects.filter(is_active=True)
-		for x in users:
-			start_date = x.date_joined.date()
-			end_date = start_date + timedelta(days=3)
-			
-			if end_date < today:
-				User.objects.get(pk=x.id).delete()
-				print(f'Just deleted {x.username}')
+    def handle(self, *arg, **kwargs):
+        users = User.objects.filter(is_active=True)
+        for user in users:
+            words_ownership = WordOwnership.objects.filter(
+                user_id=user.id).values_list('word_id', flat=True)
+            if len(words_ownership) >= 5:
+                random_5_words_onwership = random_5_words(words_ownership)
+                print(random_5_words_onwership)
+                send_summary_voca_mail(user, random_5_words_onwership)
+                print("aldready send")
+
+def random_5_words(words_ownership):
+    shuffled_words_ownership = list(words_ownership)
+    random.shuffle(shuffled_words_ownership)
+    return shuffled_words_ownership[:5]
+
+
+def send_summary_voca_mail(user, words):
+    now = datetime.now()
+    date = now.strftime("%m/%d/%Y")
+    subject = 'Daily Vocabulary Review on ' + date
+    message = format_html('Hi {}, <br>', user.first_name)
+    body = format_html(
+        "<br>It's time for your daily vocabulary review! By using active recall method to help you remember your vocabulary words for your long term memory 🧠.<br/>")
+    body += format_html('Here are the 5 vocabulary words for you to review today.<br/><ol>')
+    for word_id in words:
+        word = Word.objects.filter(pk=word_id).first()
+        body += format_html('<li><b>{}</b><br/>', word.name)
+        word_entries = WordEntry.objects.filter(word=word, user=user)
+        for word_entry in word_entries:
+
+            body += format_html('- {}: {}<br/>',
+                                word_entry.word_type, word_entry.definition)
+            body += format_html('<b>Example:</b> {}', word_entry.example)
+        body += format_html('</li>')
+    body += format_html("</ol><br><b>Please adding more vocabulary words to <a href='happydictionary.net'>this link</a> when you've learned any new words 📚 👍. It's a great way to strengthen your memory muscles 💪.</b>")
+    recipient = [user.email] 
+    email = EmailMessage(
+        subject,
+        message +
+        body,
+        'noreply@happydictionary.net',
+        recipient,
+    )
+    email.content_subtype = 'html'
+    email.send()
+    # send_mail(subject, message, email_from, recipient, html_message=body)
