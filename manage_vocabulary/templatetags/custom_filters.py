3c56f0253b7aec21fff3f29697bdf728913dd289
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-09-29 12:34:02 -0700

    use pluralize filter

--- manage_vocabulary/templatetags/custom_filters.py
+++ manage_vocabulary/templatetags/custom_filters.py
@@ -3,12 +3,8 @@ import calendar
 
 register = template.Library()
 
+
 @register.filter
 def month_name(month_number):
     month_number = int(month_number)
     return calendar.month_name[month_number]
-
-
-@register.filter
-def add_if_plural(value):
-    return f'{value} words' if value > 1 else f'{value} word'
