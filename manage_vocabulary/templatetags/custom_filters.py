69fdae878021decc70c36ae1744d5393fb9df808
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-09-29 12:16:26 -0700

    add new filter for plural

--- manage_vocabulary/templatetags/custom_filters.py
+++ manage_vocabulary/templatetags/custom_filters.py
@@ -3,11 +3,18 @@ import calendar
 
 register = template.Library()
 
+
 @register.filter
 def replacetoSpace(value):
     return value.replace('-', ' ')
+
 
 @register.filter
 def month_name(month_number):
     month_number = int(month_number)
     return calendar.month_name[month_number]
+
+
+@register.filter
+def add_if_plural(value):
+    return f'{value} words' if value > 1 else f'{value} word'
