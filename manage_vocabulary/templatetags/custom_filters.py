fc93a18d9bb724f517e3d44c3f9ae047466eebd1
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-09-29 12:17:43 -0700

    delte replacetoSpace custom filter

--- manage_vocabulary/templatetags/custom_filters.py
+++ manage_vocabulary/templatetags/custom_filters.py
@@ -3,12 +3,6 @@ import calendar
 
 register = template.Library()
 
-
-@register.filter
-def replacetoSpace(value):
-    return value.replace('-', ' ')
-
-
 @register.filter
 def month_name(month_number):
     month_number = int(month_number)
