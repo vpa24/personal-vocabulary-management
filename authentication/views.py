21248cdab37eb58088b9e53561137e1239ea1509
Vũ Phương Anh <vuphuonganh020497@gmail.com>
2023-10-07 20:06:23 -0700

    add heroku package

--- authentication/views.py
+++ authentication/views.py
@@ -5,10 +5,8 @@ from django.http import JsonResponse
 from validate_email import validate_email
 from django.contrib import messages
 from django.core.mail import EmailMessage
-from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeError
 from django.core.mail import send_mail
-from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.template.loader import render_to_string
 from .utils import account_activation_token
@@ -60,10 +58,10 @@ class RegistrationView(View):
             user.set_password(password)
             user.is_active = False
             user.save()
-            current_site = get_current_site(request)
+            current_site = request.build_absolute_uri("/")
             email_body = {
                 'user': user,
-                'domain': current_site.domain,
+                'domain': current_site,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': account_activation_token.make_token(user),
             }
@@ -73,7 +71,7 @@ class RegistrationView(View):
 
             email_subject = 'Please activate your account!'
 
-            activate_url = current_site.domain+link
+            activate_url = current_site+link
 
             email = EmailMessage(
                 email_subject,
@@ -86,7 +84,6 @@ class RegistrationView(View):
             messages.success(
                 request, 'Account successfully created. Please check your email to active your account.')
             return redirect('index')
-            # return render(request, 'authentication/register.html', {'form': form, 'include_register_script':  True})
         else:
             context = {
                 'form': form,
